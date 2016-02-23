52bdbc8b9cce4be1d9c7ab656bc2afbd871b25ab
Hatsune Miku <999Hatsune@gmail.com>
2016-02-23 15:18:08 +0900

    execute in_transaction

--- src/FirebirdDB.jl
+++ src/FirebirdDB.jl
@@ -3,7 +3,7 @@
 module FirebirdDB
 
 export FB_API_HANDLE, ISC_DB_HANDLE, ISC_TR_HANDLE, Firebird
-export connect, close
+export connect, close, in_transaction
 
 const ISC_STATUS_LENGTH = 20
 const ISC_STATUS_ARRAY_SIZE = ISC_STATUS_LENGTH * 8 * sizeof(UInt)
@@ -30,7 +30,7 @@ function Firebird(dsn::ASCIIString, usr::ASCIIString, pwd::ASCIIString,
     dsn, usr, pwd, charset)
 end
 
-function pr_error(stat::Array{UInt8, 1}, ope::ASCIIString, result::Any)
+function pr_error(stat::Array{UInt8, 1}, ope::AbstractString, result::Any)
   println("[")
   println(@sprintf "PROBLEM ON '%s'." ope)
   ccall((:isc_print_status, :fbclient), Void, (Ptr{UInt8},), stat)
@@ -45,7 +45,7 @@ function connect(dsn::ASCIIString, usr::ASCIIString, pwd::ASCIIString,
   fb = Firebird(dsn, usr, pwd, charset)
   r = ccall((:isc_attach_database, :fbclient), Bool,
     (Ptr{UInt8}, UInt16, Ptr{UInt8}, Ptr{Ptr{Void}}, UInt16, Ptr{UInt8}),
-    fb.stat, 0, fb.dsn.data, fb.cn, 0, C_NULL)
+    fb.stat, 0, [fb.dsn.data; [0x00]], fb.cn, 0, C_NULL)
   if r || (fb.cn[1] == C_NULL)
     return pr_error(fb.stat, "attach database", nothing)
   end
@@ -62,23 +62,23 @@ function close(fb::Firebird)
   return !r
 end
 
-function cleanup(fb::Firebird)
+function in_transaction(fb::Firebird, s::AbstractString)
   r = ccall((:isc_start_transaction, :fbclient), Bool,
     (Ptr{UInt8}, Ptr{Ptr{Void}}, UInt16, Ptr{Ptr{Void}}, UInt16, Ptr{UInt8}),
     fb.stat, fb.tr, 1, fb.cn, 0, C_NULL)
-  r && return pr_error(fb.stat, "cleanup: start transaction", false)
-  r = ccall((:isc_dsql_execute_immediate, :fbclient), Bool,
-    (Ptr{UInt8}, Ptr{Ptr{Void}}, Ptr{Ptr{Void}}, UInt16,
-      Ptr{UInt8},
-      UInt16, Ptr{XSQLDA}),
-    fb.stat, fb.cn, fb.tr, 0,
-      "delete from ttest where c2 in ('abc', 'def', 'xyz');",
-      1, C_NULL)
-  r && return pr_error(fb.stat, "cleanup: dsql execute immediate", false)
+  r && return pr_error(fb.stat, "in: start transaction", false)
+  # for ss in split(s, "\n") # to handle SubString convert(UTF8String, ss).data
+    r = ccall((:isc_dsql_execute_immediate, :fbclient), Bool,
+      (Ptr{UInt8}, Ptr{Ptr{Void}}, Ptr{Ptr{Void}}, UInt16,
+        Ptr{UInt8}, UInt16, Ptr{XSQLDA}),
+      fb.stat, fb.cn, fb.tr, 0, [s.data; [0x00]], 1, C_NULL)
+    r && return pr_error(fb.stat,
+      (@sprintf "in: dsql execute immediate\n [%s]" s), false)
+  # end
   r = ccall((:isc_commit_transaction, :fbclient), Bool,
     (Ptr{UInt8}, Ptr{Ptr{Void}}),
     fb.stat, fb.tr)
-  r && return pr_error(fb.stat, "cleanup: commit transaction", false)
+  r && return pr_error(fb.stat, "in: commit transaction", false)
   return true
 end
 
--- test/firebirddb_tests.jl
+++ test/firebirddb_tests.jl
@@ -6,7 +6,21 @@ println("Testing FirebirdDB")
 fb = FirebirdDB.connect("testdb1", "sysdba", "password")
 println(fb != nothing ? fb.cn :  fb)
 @test fb != nothing
-@test FirebirdDB.cleanup(fb)
+@test FirebirdDB.in_transaction(fb, "drop table ttest;")
+@test FirebirdDB.in_transaction(fb,
+  "create table ttest (id integer, c1 varchar(10), c2 varchar(20));")
+@test FirebirdDB.in_transaction(fb,
+  "insert into ttest (id, c1, c2) values (1, 'ABC', 'XYZ');")
+@test FirebirdDB.in_transaction(fb,
+  "insert into ttest (id, c1, c2) values (2, '222', '000');")
+@test FirebirdDB.in_transaction(fb,
+  "insert into ttest (id, c1, c2) values (4, '333', 'xyz');")
+@test FirebirdDB.in_transaction(fb,
+  "insert into ttest (id, c1, c2) values (11, 'ABC', 'XYZ');")
+@test FirebirdDB.in_transaction(fb,
+  "insert into ttest (id, c1, c2) values (22, '222', '000');")
+@test FirebirdDB.in_transaction(fb,
+  "delete from ttest where c2 in ('abc', 'def', 'xyz');")
 @test FirebirdDB.close(fb)
 println(fb.cn)
 println("ok")
