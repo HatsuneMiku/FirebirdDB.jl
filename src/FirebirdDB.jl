20a74f3014e8bcfcc9463661a80bc0bd13f3daee
Hatsune Miku <999Hatsune@gmail.com>
2016-02-23 11:30:06 +0900

    immutable Firebird and FB_API_HANDLE

--- src/FirebirdDB.jl
+++ src/FirebirdDB.jl
@@ -2,13 +2,27 @@
 
 module FirebirdDB
 
+export FB_API_HANDLE, Firebird
 export connect, close
 
 const ISC_STATUS_LENGTH = 20
 const ISC_STATUS_ARRAY_SIZE = ISC_STATUS_LENGTH * 8 * sizeof(UInt)
 
+typealias FB_API_HANDLE Ptr{Void}
+
 immutable Firebird
+  stat::Array{UInt8, 1}
+  cn::Array{FB_API_HANDLE, 1} # must be initialized with [C_NULL]
   dsn::ASCIIString
+  usr::ASCIIString
+  pwd::ASCIIString
+  charset::ASCIIString
+end
+
+function Firebird(dsn::ASCIIString, usr::ASCIIString, pwd::ASCIIString,
+  charset::ASCIIString="UTF8")
+  return Firebird(Array(UInt8, ISC_STATUS_ARRAY_SIZE), [C_NULL],
+    dsn, usr, pwd, charset)
 end
 
 function pr_error(stat::Array{UInt8, 1}, ope::ASCIIString)
@@ -23,20 +37,25 @@ end
 
 function connect(dsn::ASCIIString, usr::ASCIIString, pwd::ASCIIString,
   charset::ASCIIString="UTF8")
-  cn = Array{UInt, 1}([0]) # must set 0
-  stat = Array(UInt8, ISC_STATUS_ARRAY_SIZE)
+  fb = Firebird(dsn, usr, pwd, charset)
   r = ccall((:isc_attach_database, :fbclient),
-    Bool, (Ptr{UInt8}, UInt16, Ptr{UInt8}, Ptr{UInt}, UInt16, Ptr{UInt8}),
-    stat, 0, dsn.data, cn, 0, C_NULL)
-  (r || (cn[1] == 0)) && (pr_error(stat, "attach database"); return nothing)
-  return cn # Firebird(dsn)
+    Bool, (Ptr{UInt8}, UInt16, Ptr{UInt8}, Ptr{Ptr{Void}}, UInt16, Ptr{UInt8}),
+    fb.stat, 0, fb.dsn.data, fb.cn, 0, C_NULL)
+  if r || (fb.cn[1] == C_NULL)
+    pr_error(fb.stat, "attach database")
+    return nothing
+  end
+  return fb
 end
 
-function close(cn)
-  stat = Array(UInt8, ISC_STATUS_ARRAY_SIZE)
+function close(fb::Firebird)
   r = ccall((:isc_detach_database, :fbclient),
-    Bool, (Ptr{UInt8}, Ptr{Void}),
-    stat, pointer(cn))
+    Bool, (Ptr{UInt8}, Ptr{Ptr{Void}}),
+    fb.stat, fb.cn)
+  if r || (fb.cn[1] != C_NULL)
+    pr_error(fb.stat, "detach database")
+    return false
+  end
   return !r
 end
 
--- test/firebirddb_tests.jl
+++ test/firebirddb_tests.jl
@@ -3,8 +3,9 @@
 import FirebirdDB
 
 println("Testing FirebirdDB")
-cn = FirebirdDB.connect("testdb1", "sysdba", "password")
-println(cn)
-@test cn != nothing
-@test FirebirdDB.close(cn)
+fb = FirebirdDB.connect("testdb1", "sysdba", "password")
+println(fb != nothing ? fb.cn :  fb)
+@test fb != nothing
+@test FirebirdDB.close(fb)
+println(fb.cn)
 println("ok")
