70a4b083f132d72d6c2ffe589b6d6aba7679f07b
Hatsune Miku <999Hatsune@gmail.com>
2016-02-25 16:30:50 +0900

    :fbclient / :libfbclient

--- src/FirebirdDB.jl
+++ src/FirebirdDB.jl
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
+# FirebirdDB
 
+VERSION >= v"0.4.0-dev+6521" && __precompile__()
 module FirebirdDB
 
 export FB_API_HANDLE, ISC_DB_HANDLE, ISC_TR_HANDLE, Firebird
 export connect, close, in_transaction
+
+const FBCLIENT = @windows ? :fbclient : :libfbclient
 
 const ISC_STATUS_LENGTH = 20
 const ISC_STATUS_ARRAY_SIZE = ISC_STATUS_LENGTH * 8 * sizeof(UInt)
@@ -33,8 +37,8 @@ end
 function pr_error(stat::Array{UInt8, 1}, ope::AbstractString, result::Any)
   println("[")
   println(@sprintf "PROBLEM ON '%s'." ope)
-  ccall((:isc_print_status, :fbclient), Void, (Ptr{UInt8},), stat)
-  c = ccall((:isc_sqlcode, :fbclient), UInt, (Ptr{UInt8},), stat)
+  ccall((:isc_print_status, FBCLIENT), Void, (Ptr{UInt8},), stat)
+  c = ccall((:isc_sqlcode, FBCLIENT), UInt, (Ptr{UInt8},), stat)
   println(@sprintf "SQLCODE: %08x" c)
   println("]")
   return result
@@ -43,7 +47,7 @@ end
 function connect(dsn::ASCIIString, usr::ASCIIString, pwd::ASCIIString,
   charset::ASCIIString="UTF8")
   fb = Firebird(dsn, usr, pwd, charset)
-  r = ccall((:isc_attach_database, :fbclient), Bool,
+  r = ccall((:isc_attach_database, FBCLIENT), Bool,
     (Ptr{UInt8}, UInt16, Ptr{UInt8}, Ptr{Ptr{Void}}, UInt16, Ptr{UInt8}),
     fb.stat, 0, [fb.dsn.data; [0x00]], fb.cn, 0, C_NULL)
   if r || (fb.cn[1] == C_NULL)
@@ -53,7 +57,7 @@ function connect(dsn::ASCIIString, usr::ASCIIString, pwd::ASCIIString,
 end
 
 function close(fb::Firebird)
-  r = ccall((:isc_detach_database, :fbclient), Bool,
+  r = ccall((:isc_detach_database, FBCLIENT), Bool,
     (Ptr{UInt8}, Ptr{Ptr{Void}}),
     fb.stat, fb.cn)
   if r || (fb.cn[1] != C_NULL)
@@ -63,19 +67,19 @@ function close(fb::Firebird)
 end
 
 function in_transaction(fb::Firebird, s::AbstractString)
-  r = ccall((:isc_start_transaction, :fbclient), Bool,
+  r = ccall((:isc_start_transaction, FBCLIENT), Bool,
     (Ptr{UInt8}, Ptr{Ptr{Void}}, UInt16, Ptr{Ptr{Void}}, UInt16, Ptr{UInt8}),
     fb.stat, fb.tr, 1, fb.cn, 0, C_NULL)
   r && return pr_error(fb.stat, "in: start transaction", false)
   # for ss in split(s, "\n") # to handle SubString convert(UTF8String, ss).data
-    r = ccall((:isc_dsql_execute_immediate, :fbclient), Bool,
+    r = ccall((:isc_dsql_execute_immediate, FBCLIENT), Bool,
       (Ptr{UInt8}, Ptr{Ptr{Void}}, Ptr{Ptr{Void}}, UInt16,
         Ptr{UInt8}, UInt16, Ptr{XSQLDA}),
       fb.stat, fb.cn, fb.tr, 0, [s.data; [0x00]], 1, C_NULL)
     r && return pr_error(fb.stat,
       (@sprintf "in: dsql execute immediate\n [%s]" s), false)
   # end
-  r = ccall((:isc_commit_transaction, :fbclient), Bool,
+  r = ccall((:isc_commit_transaction, FBCLIENT), Bool,
     (Ptr{UInt8}, Ptr{Ptr{Void}}),
     fb.stat, fb.tr)
   r && return pr_error(fb.stat, "in: commit transaction", false)
